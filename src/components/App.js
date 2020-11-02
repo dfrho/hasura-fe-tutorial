79124f9f729d88044d4edb5f3c82166ff2a4c348
David Rhodes <david@espressocode.tech>
2020-11-02 06:40:47 -0600

    feat(subs): adds subs for online users

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,22 @@ import TodoPrivateWrapper from './Todo/TodoPrivateWrapper';
 import TodoPublicWrapper from './Todo/TodoPublicWrapper';
 import OnlineUsersWrapper from './OnlineUsers/OnlineUsersWrapper';
 
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  HttpLink,
-} from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
 import { useAuth0 } from './Auth/react-auth0-spa';
 
 const creatApolloClient = authToken => {
   return new ApolloClient({
-    link: new HttpLink({
-      uri: 'https://hasura.io/learn/graphql',
-      headers: {
-        Authorization: `Bearer ${authToken}`,
+    link: new WebSocketLink({
+      uri: 'wss://hasura.io/learn/graphql',
+      options: {
+        reconnect: true,
+        connectionParams: {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        },
       },
     }),
     cache: new InMemoryCache(),
--- src/components/OnlineUsers/OnlineUsersWrapper.js
+++ src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -1,19 +1,65 @@
-import React from "react";
-
-import OnlineUser from "./OnlineUser";
+import React, { useEffect, Fragment, useState } from 'react';
+import { useMutation, useSubscription, gql } from '@apollo/client';
+import OnlineUser from './OnlineUser';
 
 const OnlineUsersWrapper = () => {
-  const onlineUsers = [{ name: "someUser1" }, { name: "someUser2" }];
+  const [onlineIndicator, setOnlineIndicator] = useState(0);
+  let onlineUsersList = [];
+  useEffect(() => {
+    updateLastSeen();
+    setOnlineIndicator(setInterval(() => updateLastSeen(), 3000));
+    return () => {
+      clearInterval(onlineIndicator);
+    };
+  }, []);
 
-  const onlineUsersList = [];
-  onlineUsers.forEach((user, index) => {
-    onlineUsersList.push(<OnlineUser key={index} index={index} user={user} />);
-  });
+  const UPDATE_LASTSEEN_MUTATION = gql`
+    mutation updateLastSeen($now: timestamptz!) {
+      update_users(where: {}, _set: { last_seen: $now }) {
+        affected_rows
+      }
+    }
+  `;
+  const [updateLastSeenMutation] = useMutation(UPDATE_LASTSEEN_MUTATION);
+  const updateLastSeen = () => {
+    updateLastSeenMutation({
+      variables: { now: new Date().toISOString() },
+    });
+  };
+
+  const { loading, error, data } = useSubscription(
+    gql`
+      subscription getOnlineUsers {
+        online_users(order_by: { user: { name: asc } }) {
+          id
+          user {
+            name
+          }
+        }
+      }
+    `
+  );
+  if (loading) {
+    return <span>Loading...</span>;
+  }
+  if (error) {
+    console.error(error);
+    return <span>Error!</span>;
+  }
+  if (data) {
+    onlineUsersList = data.online_users.map(user => (
+      <OnlineUser key={user.id} user={user.user} />
+    ));
+  }
 
   return (
     <div className="onlineUsersWrapper">
-      <div className="sliderHeader">Online users - {onlineUsers.length}</div>
-      {onlineUsersList}
+      <Fragment>
+        <div className="sliderHeader">
+          Online user - {onlineUsersList.length}
+        </div>
+        {onlineUsersList}
+      </Fragment>
     </div>
   );
 };
